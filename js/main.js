baa6c430e38d49f64e94c8080e9dab8f2aa335e1
AndreaV <a.vannetti08@gmail.com>
2020-01-30 17:07:24 +0100

    last changes

--- js/main.js
+++ js/main.js
@@ -7,9 +7,7 @@ const projectsArray = [
         siteURL: "https://andreav89.github.io/Project-8/",
         codeURL: "https://github.com/AndreaV89/Project-8",
         skills: "#AJAX, #Javascript, #JQuery, #API, #CSS",
-        description: "Startup Employee Directory. In this project I used Random User Generator API to grab information \
-                    for 12 random employees, and used fetch and parse to listing the employees in a grid with their image, \
-                    full name and location. Clicking the employee's image will open a modal window with more detailed information."
+        description: "Startup Employee Directory. In this project I used Random User Generator API to grab information for 12 random employees, and used fetch and parse to listing the employees in a grid with their image, full name and location. Clicking the employee's image will open a modal window with more detailed information."
     },
     {
         name: "WebApp Dashboard",
@@ -17,9 +15,7 @@ const projectsArray = [
         siteURL: "https://andreav89.github.io/Project-7/",
         codeURL: "https://github.com/AndreaV89/Project-7",
         skills: "#HTML, #CSS, #GridLayout, #SVG, #JQuery",
-        description: "Interactive dashboard for a web application. HTML and CSS was created from scratch looking a mockup. \
-                    This project is full of awesome features like notifications panel, autocomplete searching of users, \
-                    implementation of chart plugin, and Local Storage for saving settings."
+        description: "Interactive dashboard for a web application. HTML and CSS was created from scratch looking a mockup. This project is full of awesome features like notifications panel, autocomplete searching of users, implementation of chart plugin, and Local Storage for saving settings."
     },
     {
         name: "Wheel of Success",
@@ -27,9 +23,7 @@ const projectsArray = [
         siteURL: "https://andreav89.github.io/Project-6/",
         codeURL: "https://github.com/AndreaV89/Project-6",
         skills: "#Javascript, #JQuery",
-        description: "Browser version of a word guessing game. Using Javascript i've created an array of casual phrases. \
-                    With a function the program will choose a phrase from that array, split the phrase into letters, and put \
-                    those letters onto the game board."
+        description: "Browser version of a word guessing game. Using Javascript i've created an array of casual phrases. With a function the program will choose a phrase from that array, split the phrase into letters, and put those letters onto the game board."
     },
     {
         name: "Interactive Photo Gallery",
@@ -37,8 +31,7 @@ const projectsArray = [
         siteURL: "https://andreav89.github.io/Project-5/",
         codeURL: "https://github.com/AndreaV89/Project-5",
         skills: "#Javascript, #JQuery, #CSS, #HTML",
-        description: "This is a simple image gallery. Clicking on images will open a lightbox with larger image and caption.\
-                    The user can search for an image using the search box, that filter the images in real time."
+        description: "This is a simple image gallery. Clicking on images will open a lightbox with larger image and caption. The user can search for an image using the search box, that filter the images in real time."
     },
     {
         name: "Online Registration Form",
@@ -46,8 +39,7 @@ const projectsArray = [
         siteURL: "https://andreav89.github.io/Project-3",
         codeURL: "https://github.com/AndreaV89/Project-3",
         skills: "#HTML, #CSS",
-        description: "Really simple online registration form example. Is fully responsive and mobile-friendly. Here is a \
-                    wide variety of HTML form input types, like text input, checkboxes, radio buttons, select menu and so on."
+        description: "Really simple online registration form example. Is fully responsive and mobile-friendly. Here is a wide variety of HTML form input types, like text input, checkboxes, radio buttons, select menu and so on."
     }
 ];
 
@@ -81,7 +73,7 @@ $(document).ready(function() {
         createModal(index);
         $('#modal').animate({
             right: '0'
-        }, 600)
+        }, 500);
     });
 });
 
@@ -89,7 +81,7 @@ $('#modal').on('click', '#exit-modal', function() {
     modalHTML = "";
     $('#modal').animate({
         right: '-100vw'
-    }, 600)
+    }, 500);
 });
 
 // Slide Menu
@@ -99,18 +91,10 @@ $(document).ready(function(){
         $('.main-nav').slideToggle(200);
         $('.main-nav').css('display' , 'flex');
     });
-})
-
-// PagePiling Plugin
-
-$(document).ready(function() {
-    $('#pagepiling').pagepiling({
-        menu: '#menu',
-        anchors: ['homepage','projects','about','contact'],
-        sectionsColor: ['#222222','#222222','#222222','#222222'],
-        loopBottom: true,
-    });
 });
+
+
+
 
 
 
